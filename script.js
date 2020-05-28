5727de6b21043d4af6bd98e2c7b1cb533a68a72a
Joyce Wu <jianjoycewu@gmail.com>
2020-05-28 04:17:33 -0700

    works now, will add the difficulty

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function Map(atom, xnum, ynum) {
                     a.style.cssText = "border: 1px solid yellow";
                     a.style.width = this.atom + 'px';
                     a.style.height = this.atom + 'px';
-                    a.style.backgroundColor = "skyblue";
+                    a.style.backgroundColor = "lightgrey";
                     this.canvas.appendChild(a);
                     a.style.position = "absolute";
                     a.style.left = x * this.atom + 'px';
@@ -86,23 +86,24 @@ function Snake(map) {
     // a method to display the snake
     this.display = function() {
         for (let i = 0; i < this.body.length; i++) {
-            if (this.body[i].x != null) // when snake eats the food, x === null, 
-            var s = document.createElement('div');
-            this.body[i].flag = s;
-            
-            // define the style of the snake 
-            s.style.width = this.width + 'px';
-            s.style.height = this.height + 'px';
+            if (this.body[i].x != null) { // when snake eats the food, x === null, 
+                var s = document.createElement('div');
+                this.body[i].flag = s;
+                
+                // define the style of the snake 
+                s.style.width = this.width + 'px';
+                s.style.height = this.height + 'px';
 
-            s.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 240) + ", " + Math.floor(Math.random() * 240) + ", " + Math.floor(Math.random() * 240) + ")";
+                s.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 240) + ", " + Math.floor(Math.random() * 240) + ", " + Math.floor(Math.random() * 240) + ")";
 
-            // set the position 
-            s.style.position = 'absolute';
-            s.style.left = this.body[i].x * this.width + 'px';
-            s.style.top = this.body[i].y * this.height + 'px';
+                // set the position 
+                s.style.position = 'absolute';
+                s.style.left = this.body[i].x * this.width + 'px';
+                s.style.top = this.body[i].y * this.height + 'px';
 
-            // append the element to the map 
-            map.canvas.appendChild(s);
+                // append the element to the map 
+                map.canvas.appendChild(s);
+            }
         }
     }
 
@@ -121,6 +122,34 @@ function Snake(map) {
             case 'down': this.body[0].y += 1; break
         }
 
+        // check if the snake eats the food 
+        if (this.body[0].x === food.x && this.body[0].y === food.y) {
+            //snake is added by one more atom for the tail
+            this.body.push({x: null, y: null, flag: null});
+            map.canvas.removeChild(food.flag);
+            food = new Food(map);
+        } 
+
+        // check if the snake hits the border of the canvas 
+        if (this.body[0].x < 0 || this.body[0].x > map.xmum - 1 || this.body[0].y < 0 || this.body[0].y > map.ynum - 1) {
+            clearInterval(timer); // clear the timer 
+            alert('GAME OVER');
+
+            restart(map, this);
+            return false;
+        }
+
+        // check if the snake reverse or the snake hits its body 
+        for (let i = 4; i < this.body.length; i++) {
+            if (this.body[0].x === this.body[i].x && this.body[0].y === this.body[i].y){
+                clearInterval(timer);
+                alert('GAME OVER');
+
+                restart(map, this);
+                return false;
+            }
+        }
+
         for (let i = 0; i < this.body.length; i++) {
             if (this.body[i].flag != null) { // when snake eats the food, flag equals to null, and cannot be deleted
                 map.canvas.removeChild(this.body[i].flag);
@@ -129,6 +158,25 @@ function Snake(map) {
 
         this.display();
     }
+}
+
+// re-start the game 
+function restart(map, snake) {
+    for (var i = 0; i < snake.body.length; i++) {
+        map.canvas.removeChild(snake.body[i].flag);
+    }
+
+    snake.body = [
+        {x: 2, y: 0},
+        {x: 1, y: 0},
+        {x: 0, y: 0}
+    ];
+
+    snake.direction = 'right';
+    snake.display();
+
+    map.canvas.removeChild(food.flag);
+    food = new Food(map);
 }
 
 // create the instance of Map
@@ -164,7 +212,7 @@ window.onkeydown = function(e) {
             }
             break;
         case 39: 
-            if (snake.direction != 'down') {
+            if (snake.direction != 'left') {
                 snake.direction = 'right';
             }
             break;
